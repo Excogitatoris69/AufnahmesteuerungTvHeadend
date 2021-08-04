214d0e557db0af76a89c9af8704bd2c738b40430
Oli Mat <infoolimat69@t-online.de>
2021-08-04 22:37:53 +0200

    Projektdateien hinzufügen.

---
+++ AufnahmesteuerungTvHeadendClient.cs
@@ -0,0 +1,146 @@
+﻿using CommandLine;
+using System;
+using TvHeadendRestApiClientLibrary;
+
+namespace AufnahmesteuerungTvHeadend
+{
+    class AufnahmesteuerungTvHeadendClient
+    {
+        static void Main(string[] args)
+        {
+            Execute(args);
+        }
+
+
+        static private void Execute(string[] args)
+        {
+            RequestData requestData = new RequestData();
+            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+
+            ParserResult<CommandLineOptions> parseResult = result.WithParsed<CommandLineOptions>(cmdParam =>
+            {
+                requestData.Command = cmdParam.Command;
+                requestData.ServerUrl = cmdParam.Serverurl;
+                requestData.UserName = cmdParam.Username;
+                requestData.Password = cmdParam.Password;
+                requestData.Starttime = cmdParam.Starttime;
+                requestData.Endtime = cmdParam.Endtime;
+                requestData.ChannelName = cmdParam.Channelname;
+                requestData.Title = cmdParam.Title;
+                requestData.Description = cmdParam.Description;
+                requestData.Uuid = cmdParam.Uuid;
+                requestData.Language = cmdParam.Language;
+                requestData.DvrProfileName = cmdParam.DvrProfileName;
+                requestData.Comment = cmdParam.DvrComment;
+                requestData.Priority = Priority.Unknown;
+            });
+            if (parseResult.Tag == ParserResultType.NotParsed)
+            {
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                if (requestData.Command == Command.DVRCREATE)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    TvHeadendResponseData aTvHeadendResponseData = aTvHeadendLibrary.AddDvrEntry(requestData);
+
+                    if (aTvHeadendResponseData != null)
+                    {
+                        Console.WriteLine("Successful. UUID:{0}", aTvHeadendResponseData.Uuid);
+                    }
+                }
+                else if (requestData.Command == Command.DVRREMOVE)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    TvHeadendResponseData aTvHeadendResponseData = aTvHeadendLibrary.RemoveDvrEntry(requestData);
+                    if (aTvHeadendResponseData != null)
+                    {
+                        Console.WriteLine("Successful. UUID:{0}", aTvHeadendResponseData.Uuid);
+                    }
+                }
+                else if (requestData.Command == Command.DVRLIST)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    DvrUpcomingEntryList dvrUpcomingEntryList = aTvHeadendLibrary.GetDvrUpcominglist(requestData);
+                    if (dvrUpcomingEntryList.Entries != null && dvrUpcomingEntryList.Entries.Count > 0)
+                    {
+                        foreach (DvrUpcomingEntry elem in dvrUpcomingEntryList.Entries)
+                        {
+                            Console.WriteLine("\"{0}\",{1},{2},{3},{4},{5}", elem.Title.GetFirstTitle(), elem.Channelname, 
+                                FormatUnixtimestamp(elem.Start), FormatUnixtimestamp(elem.Stop), elem.Owner, elem.Uuid);
+                        }
+                    }
+                }
+                else if (requestData.Command == Command.CHANNELLIST)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    ChannelEntryList channelEntryList = aTvHeadendLibrary.GetChannellist(requestData);
+                    if (channelEntryList.Entries != null && channelEntryList.Entries.Count > 0)
+                    {
+                        foreach (ChannelEntry elem in channelEntryList.Entries)
+                        {
+                            Console.WriteLine("{0}", elem.Name);
+                        }
+                    }
+                }
+                else if (requestData.Command == Command.DVRCONFIGLIST)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    DvrConfigEntryList dvrConfigEntryList = aTvHeadendLibrary.GetDvrConfiglist(requestData);
+                    if (dvrConfigEntryList.Entries != null && dvrConfigEntryList.Entries.Count > 0)
+                    {
+                        foreach (DvrConfigEntry elem in dvrConfigEntryList.Entries)
+                        {
+                            Console.WriteLine("{1}: {0}", elem.Name, elem.Uuid);
+                        }
+                    }
+                }
+                else if (requestData.Command == Command.LANGUAGELIST)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    LanguageEntryList languageEntryList = aTvHeadendLibrary.GetLanguagelist(requestData);
+                    if (languageEntryList.Entries != null && languageEntryList.Entries.Count > 0)
+                    {
+                        foreach (LanguageEntry elem in languageEntryList.Entries)
+                        {
+                            Console.WriteLine("{0}: {1}", elem.Shortname, elem.Longname);
+                        }
+                    }
+                }
+                else if (requestData.Command == Command.SERVERINFO)
+                {
+                    TvHeadendLibrary aTvHeadendLibrary = new TvHeadendLibrary();
+                    Serverinfo serverinfo = aTvHeadendLibrary.GetServerinfo(requestData);
+                    Console.WriteLine("API:{0},TvHeadend:{1}", serverinfo.VersionApi, serverinfo.VersionSoftware);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    throw new TvHeadendException(Messages.MESSAGE_INVALID_COMMAND + ": " + requestData.Command);
+                }
+
+            }
+            catch (TvHeadendException e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+
+        }
+
+        private static string FormatUnixtimestamp(double unixtimestamp)
+        {
+            return UnixTimeStampToDateTime(unixtimestamp).ToString("HH:mm dd.MM.yyyy");
+        }
+
+        private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
---
+++ CommandLineOptions.cs
@@ -0,0 +1,47 @@
+﻿using CommandLine;
+
+namespace AufnahmesteuerungTvHeadend
+{
+    public class CommandLineOptions
+    {
+        [Option(shortName: 'a', longName: "command", HelpText = "Command", Required = true)]
+        public string Command { get; set; }
+
+        [Option(shortName: 's', longName: "serverurl", HelpText = "Server-URL", Required = true)]
+        public string Serverurl { get; set; }
+
+        [Option(shortName: 'u', longName: "username", HelpText = "Username", Required = true)]
+        public string Username { get; set; }
+
+        [Option(shortName: 'p', longName: "password", HelpText = "Password", Required = true)]
+        public string Password { get; set; }
+
+        [Option(shortName: 'b', longName: "starttime", HelpText = "Starttime (Unixtime)", Required = false)]
+        public long Starttime { get; set; }
+
+        [Option(shortName: 'e', longName: "endtime", HelpText = "Endtime (Unixtime)", Required = false)]
+        public long Endtime { get; set; }
+
+        [Option(shortName: 'c', longName: "channel", HelpText = "Channelname", Required = false)]
+        public string Channelname { get; set; }
+
+        [Option(shortName: 't', longName: "title", HelpText = "Title", Required = false)]
+        public string Title { get; set; }
+
+        [Option(shortName: 'd', longName: "description", HelpText = "Description", Required = false)]
+        public string Description { get; set; }
+
+        [Option(shortName: 'i', longName: "uuid", HelpText = "UUID", Required = false)]
+        public string Uuid { get; set; }
+
+        [Option(shortName: 'l', longName: "language", HelpText = "Language", Required = false)]
+        public string Language { get; set; }
+
+        [Option(shortName: 'f', longName: "config", HelpText = "DVR-Configname", Required = false)]
+        public string DvrProfileName { get; set; }
+
+        [Option(shortName: 'o', longName: "comment", HelpText = "DVR-Comment", Required = false)]
+        public string DvrComment { get; set; }
+
+    }
+}
